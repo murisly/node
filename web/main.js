87bb5fd75bc153aa9dd7d3e9b876cce0dbb751a7
murisly <murisly2015@gmail.com>
2016-08-22 18:44:45 +0800

    node test

---
+++ web/main.js
@@ -0,0 +1,11 @@
+'usr strict';
+
+var g = global.process;
+
+var m = require('./math');
+
+var a = m.add(4 ,5);
+
+var test = "hello!";
+
+console.log(a);
---
+++ web/math.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+exports.add = add;
+exports.sub = sub;
