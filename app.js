f9ffc9a050460cddedd31123bcfe56d876a3e01b
murisly <murisly2015@gmail.com>
2016-09-02 11:45:43 +0800

    format

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ var express = require('express');
 var app = express();
 var math = require('./controller/math');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res) {
+    res.send('Hello World!');
 });
 
-app.get('/test', function (req, res) {
-	var a = new Object();
-	a.add = math.add;
-  res.send(a.add(4, 5).toString());
+app.get('/test', function(req, res) {
+    var a = new Object();
+    a.add = math.add;
+    res.send(a.add(4, 5).toString());
 });
 
-var server = app.listen(5858, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+var server = app.listen(5858, function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 });
