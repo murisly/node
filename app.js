293b8245f73975eb973a3c84683b041ce19ab2c2
murisly <murisly2015@gmail.com>
2016-09-02 11:38:03 +0800

    a object test

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-
 var express = require('express');
 var app = express();
+var math = require('./controller/math');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!！');
+  res.send('Hello World!');
+});
+
+app.get('/test', function (req, res) {
+	var a = new Object();
+	a.add = math.add;
+  res.send(a.add(4, 5).toString());
 });
 
 var server = app.listen(5858, function () {
